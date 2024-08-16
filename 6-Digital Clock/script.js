5c8f3531daf03ac403932ddec7df203c00176643
zaniarkarimi <zaniarkarimi@chmail.ir>
2024-08-16 11:27:38 +0330

    Added the period identifier for the clock and corresponding if case to show whether its "AM" or "PM", and a few other conditions.

--- 6-Digital Clock/script.js
+++ 6-Digital Clock/script.js
@@ -3,4 +3,23 @@ setInterval(() => {
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
+
+    var period = "AM";
+    if (hours >= 12) {
+        period = "PM";
+    }
+    if (hours > 12) {
+        hours -= 12;
+    }
+    if (hours === 0) { // Handle midnight case
+        hours = 12;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + period;
 }, 1000);
