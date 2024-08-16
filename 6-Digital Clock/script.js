9ed7b015b337d72c86cfd913eb0c92a0baffad49
zaniarkarimi <zaniarkarimi@chmail.ir>
2024-08-16 11:26:01 +0330

    Added the necessary javascript variables to store the time data!

---
+++ 6-Digital Clock/script.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+}, 1000);
